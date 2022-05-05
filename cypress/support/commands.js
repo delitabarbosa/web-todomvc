66700cbab52f1f2cd4c05892339e0f1191987871
Delita Barbosa <87039569+delitabarbosa@users.noreply.github.com>
2022-05-05 16:25:19 -0300

    Ajuste Pipeline
    Authored-by: Delita Barbosa da Silva

---
+++ cypress/support/commands.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+import "cypress-keycloak-commands";
+import "cypress-localstorage-commands";
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
+
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("saveLocalStorage", () => {
+    Object.keys(localStorage).forEach(key => {
+        LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+    });
+});
+
+Cypress.Commands.add("restoreLocalStorage", () => {
+    Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
+        localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+    });
+});
